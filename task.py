27e818836060bfc66254f038727d675fdb30da88
JonahEgashira <jonahega@gmail.com>
2024-04-30 18:11:04 +0900

    format

--- task.py
+++ task.py
@@ -5,22 +5,24 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPainter, QPen, QColor
 from PyQt5.QtCore import Qt, QEvent
 
+
 class Point:
     def __init__(self, index, x, y, score):
         self.index = index
         self.x = x
         self.y = y
         self.score = score
-    
+
     def __repr__(self):
         return f"Point({self.index}, {self.x}, {self.y}, {self.score})"
+
 
 class PatternLearningTask(QWidget):
     def __init__(self, x_line_num, y_line_num):
         super().__init__()
         self.x_line_num = x_line_num
         self.y_line_num = y_line_num
-        self.cell_size = 120 # Size of each cell
+        self.cell_size = 120  # Size of each cell
         self.width = None
         self.height = None
         self.points = None
@@ -28,12 +30,12 @@ class PatternLearningTask(QWidget):
 
     def initUI(self):
         self.showFullScreen()
-        self.setWindowTitle('Pattern Learning Task')
+        self.setWindowTitle("Pattern Learning Task")
         self.setStyleSheet("background-color: white;")  # Set background color to white
 
         screen = self.screen()
         size = screen.size()
-        
+
         self.width = size.width()
         self.height = size.height()
 
@@ -54,30 +56,30 @@ class PatternLearningTask(QWidget):
 
         mid_x = self.width // 2
         mid_y = self.height // 2
-        
+
         start_x = mid_x - (self.x_line_num - 1) // 2 * self.cell_size
         start_y = mid_y - (self.y_line_num - 1) // 2 * self.cell_size
-        
+
         for i in range(self.x_line_num):
-            sx = start_x + i * self.cell_size # 線のX座標始点
-            sy = start_y - self.cell_size # 線のY座標始点
-            gx = start_x + i * self.cell_size # 線のX座標終点
-            gy = start_y + self.cell_size * self.y_line_num # 線のY座標終点
+            sx = start_x + i * self.cell_size  # 線のX座標始点
+            sy = start_y - self.cell_size  # 線のY座標始点
+            gx = start_x + i * self.cell_size  # 線のX座標終点
+            gy = start_y + self.cell_size * self.y_line_num  # 線のY座標終点
 
             qp.drawLine(sx, sy, gx, gy)
-        
+
         for i in range(self.y_line_num):
             sx = start_x - self.cell_size
             sy = start_y + i * self.cell_size
             gx = start_x + self.cell_size * self.x_line_num
             gy = start_y + i * self.cell_size
-            
+
             qp.drawLine(sx, sy, gx, gy)
-    
+
     def generateIntersectionPoints(self) -> None:
         mid_x = self.width // 2
         mid_y = self.height // 2
-        
+
         start_x = mid_x - (self.x_line_num - 1) // 2 * self.cell_size
         start_y = mid_y - (self.y_line_num - 1) // 2 * self.cell_size
 
@@ -85,7 +87,9 @@ class PatternLearningTask(QWidget):
 
         # Generate list of points from 1 to point_num
         point_scores = list(range(1, point_num + 1))
-        shuffled_point_scores = random.sample(point_scores, point_num) # Shuffle the list
+        shuffled_point_scores = random.sample(
+            point_scores, point_num
+        )  # Shuffle the list
 
         intersection_points: List[Point] = []
         index = 0
@@ -93,17 +97,19 @@ class PatternLearningTask(QWidget):
             for j in range(self.y_line_num):
                 x = start_x + i * self.cell_size
                 y = start_y + j * self.cell_size
-                score = shuffled_point_scores.pop() # Pop the last element from the list
-                
+                score = (
+                    shuffled_point_scores.pop()
+                )  # Pop the last element from the list
+
                 intersection_points.append(Point(index, x, y, score))
                 index += 1
-        
+
         self.points = intersection_points
         print(self.points)
-        return 
-        
+        return
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
     x_line_num = 4
     y_line_num = 4
